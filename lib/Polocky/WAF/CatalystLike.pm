7b88aaadd1b55607629b917b31a1ed5b086004d3
polocky <polocky@cpan.org>
2010-04-20 23:00:17 +0900

    initial commit

---
+++ lib/Polocky/WAF/CatalystLike.pm
@@ -0,0 +1,27 @@
+package Polocky::WAF::CatalystLike;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Polocky::WAF::CatalystLike -
+
+=head1 SYNOPSIS
+
+  use Polocky::WAF::CatalystLike;
+
+=head1 DESCRIPTION
+
+Polocky::WAF::CatalystLike is
+
+=head1 AUTHOR
+
+Default Name E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=cut
